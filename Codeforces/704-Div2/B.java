6c12b1c736c2e1bf74e4d397f22304b7f3ee9e8e
mrprince88 <mrprince0088@gmail.com>
2021-02-24 16:48:42 +0530

    Added files

---
+++ Codeforces/704-Div2/B.java
@@ -0,0 +1,118 @@
+import java.io.*;
+import java.util.*;
+
+public class B {
+
+
+	public static void main(String[] args)throws IOException {
+
+		InputReader in = new InputReader();
+		PrintWriter pw = new PrintWriter(System.out);
+		int t = in.nextInt();
+		while (t-- > 0) {
+			int n = in.nextInt();
+			int []a = in.readArray(n);
+			int low = 0, high = -1, lar = a[0];
+			for (int i = 0; i < n; i++) {
+				if (a[i] > lar) {
+					high = i - 1;
+					rev(a, low, high);
+					lar = a[i];
+					low = i;
+				}
+			}
+
+			high = n - 1;
+			rev(a, low, high);
+			rev(a, 0, n - 1);
+
+			for (int i : a)
+				pw.print(i + " ");
+			pw.println();
+		}
+
+		pw.close();
+	}
+
+	static void rev(int []a, int i, int j) {
+		while (j > i) {
+			int t = a[i];
+			a[i] = a[j];
+			a[j] = t;
+			i++; j--;
+		}
+	}
+
+	static final Random random = new Random();
+
+	static void ruffleSort(int[] a) {
+		int n = a.length;
+		for (int i = 0; i < n; i++) {
+			int oi = random.nextInt(n), temp = a[oi];
+			a[oi] = a[i];
+			a[i] = temp;
+		}
+		ArrayList<Integer> lst = new ArrayList<>();
+		for (int i : a)
+			lst.add(i);
+		Collections.sort(lst);
+		for (int i = 0; i < n; i++)
+			a[i] = lst.get(i);
+	}
+
+	static class InputReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public InputReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		int[] readArray(int n) {
+			int []a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = nextInt();
+			return a;
+		}
+		long[] readLongArray(int n) {
+			long []a = new long[n];
+			for (int i = 0; i < n; i++)
+				a[i] = nextLong();
+			return a;
+		}
+		int [][] read2dArray(int n, int m) {
+			int [][]a = new int[n][m];
+			for (int i = 0; i < n; i++)
+				for (int j = 0; j < m; j++)
+					a[i][j] = nextInt();
+			return a;
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
