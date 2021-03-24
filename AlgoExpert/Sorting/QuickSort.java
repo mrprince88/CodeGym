c08278e41978a5e36befb03ba93ba0dcd3bf2e30
mrprince88 <mrprince0088@gmail.com>
2021-03-24 23:45:27 +0530

    Added files

--- AlgoExpert/Sorting/QuickSort.java
+++ AlgoExpert/Sorting/QuickSort.java
@@ -1,38 +1,39 @@
 class QuickSort {
 
-    static void quickSort(int []a, int l, int r) {
-        if (l >= r)
-            return;
+	static void quickSort(int []a, int l, int r) {
+		if (l >= r)
+			return;
 
-        int pivot = l;
-        int leftIndex = l + 1;
-        int rightIndex = r;
+		swap(a, l, (l + r) / 2);
+		int pivot = l;
+		int leftIndex = l + 1;
+		int rightIndex = r;
 
-        while (rightIndex >= leftIndex) {
-            if (a[rightIndex] < a[pivot] && a[leftIndex] > a[rightIndex])
-                swap(a, leftIndex, rightIndex);
-            if (a[leftIndex] <= a[pivot])
-                leftIndex++;
-            if (a[rightIndex] >= a[pivot])
-                rightIndex--;
-        }
+		while (rightIndex >= leftIndex) {
+			if (a[rightIndex] < a[pivot] && a[leftIndex] > a[rightIndex])
+				swap(a, leftIndex, rightIndex);
+			if (a[leftIndex] <= a[pivot])
+				leftIndex++;
+			if (a[rightIndex] >= a[pivot])
+				rightIndex--;
+		}
 
-        swap(a, pivot, rightIndex);
-        quickSort(a, l, rightIndex - 1);
-        quickSort(a, rightIndex + 1, r);
-    }
+		swap(a, pivot, rightIndex);
+		quickSort(a, l, rightIndex - 1);
+		quickSort(a, rightIndex + 1, r);
+	}
 
-    static void swap(int []a, int l, int r) {
-        int t = a[l];
-        a[l] = a[r];
-        a[r] = t;
-    }
+	static void swap(int []a, int l, int r) {
+		int t = a[l];
+		a[l] = a[r];
+		a[r] = t;
+	}
 
-    public static void main(String[] args)  {
-        int []a = {5, 3, 1, 4, 2};
-        quickSort(a, 0, 4);
-        for (int i = 0; i < 5; i++)
-            System.out.println(a[i]);
-    }
+	public static void main(String[] args)  {
+		int []a = {5, 3, 1, 4, 2};
+		quickSort(a, 0, 4);
+		for (int i = 0; i < 5; i++)
+			System.out.println(a[i]);
+	}
 }
 
